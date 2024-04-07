b7859ca8ea58b4df071e5f71a99f3977b6519292
Rufus97 <manuelgomez@outlook.it>
2024-04-07 17:50:20 +0200

    First commit

---
+++ src/main/java/com/medteamb/medteamb/MedteambApplication.java
@@ -0,0 +1,13 @@
+package com.medteamb.medteamb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedteambApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedteambApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medteamb/medteamb/MedteambApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medteamb.medteamb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedteambApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
