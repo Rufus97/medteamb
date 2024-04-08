38a3ea7e49718e9b12f54fdc9171dfbd75d6bfe6
dmanu <dmanu2804@gmail.com>
2024-04-08 21:34:27 +0200

    fix(missing annotations)
    added @Repository and @Service

--- src/main/java/com/medteamb/medteamb/repository/PatientRepository.java
+++ src/main/java/com/medteamb/medteamb/repository/PatientRepository.java
@@ -1,9 +1,11 @@
 package com.medteamb.medteamb.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.medteamb.medteamb.model.Patient;
 
+@Repository
 public interface PatientRepository extends CrudRepository<Patient, Integer> {
 
 }
--- src/main/java/com/medteamb/medteamb/service/PatientServiceImpl.java
+++ src/main/java/com/medteamb/medteamb/service/PatientServiceImpl.java
@@ -1,8 +1,11 @@
 package com.medteamb.medteamb.service;
 
+import org.springframework.stereotype.Service;
+
 import com.medteamb.medteamb.model.Patient;
 import com.medteamb.medteamb.repository.PatientRepository;
 
+@Service
 public class PatientServiceImpl implements PatientService{
 	
 private PatientRepository patientRepository;
