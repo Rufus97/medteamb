bc9403e0b3310df31c63ff69b7189c56868ce37b
dmanu <dmanu2804@gmail.com>
2024-04-10 16:52:05 +0200

    feat(Dto)added Dto
    doctordto and mapper

--- src/main/java/com/medteamb/medteamb/service/dto/doctor/DoctorRequestDTO.java
+++ src/main/java/com/medteamb/medteamb/service/dto/doctor/DoctorRequestDTO.java
@@ -1,4 +1,23 @@
 package com.medteamb.medteamb.service.dto.doctor;
 
+import com.medteamb.medteamb.model.Specialization;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class DoctorRequestDTO {
+	
+	private Integer doctorID;
+	private String doctorName;
+	private String doctorSurname;
+	private String doctorPhoneNumber;
+	private String doctorEmail;
+	private Specialization specialization;
+	
 }
