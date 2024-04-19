5c7e0d5840a198d679de50bdf1928682c90952cb
Marija Milasinovic <30419423+mmilasi@users.noreply.github.com>
2024-04-19 19:09:18 +0200

    feature(secretary): completed service, dto and controller + other minor fixes + authorization layer deleted
    - completed basic service, dto and controller classes for secretary
    - minor fixes in patient and user model classes
    - deleted authorization layer to avoid unnecessary problems with basic code writing and testing, will be implemented further on (preserved copy in local)

--- src/main/java/com/medteamb/medteamb/model/Patient.java
+++ src/main/java/com/medteamb/medteamb/model/Patient.java
@@ -1,13 +1,9 @@
 package com.medteamb.medteamb.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
 @Entity
 @Table(name = "patient")
 public class Patient {
@@ -22,5 +18,68 @@ public class Patient {
 	private String patientPhoneNumber;
 	private String patientEmail;
 
+//	@OneToOne
+//	@JoinColumn(name = "user_id")
+//	private User user;
 
+	public Integer getPatientID() {
+		return patientID;
+	}
+
+	public void setPatientID(Integer patientID) {
+		this.patientID = patientID;
+	}
+
+	public String getPatientName() {
+		return patientName;
+	}
+
+	public void setPatientName(String patientName) {
+		this.patientName = patientName;
+	}
+
+	public String getPatientSurname() {
+		return patientSurname;
+	}
+
+	public void setPatientSurname(String patientSurname) {
+		this.patientSurname = patientSurname;
+	}
+
+	public String getTaxCode() {
+		return taxCode;
+	}
+
+	public void setTaxCode(String taxCode) {
+		this.taxCode = taxCode;
+	}
+
+	public String getPatientPhoneNumber() {
+		return patientPhoneNumber;
+	}
+
+	public void setPatientPhoneNumber(String patientPhoneNumber) {
+		this.patientPhoneNumber = patientPhoneNumber;
+	}
+
+	public String getPatientEmail() {
+		return patientEmail;
+	}
+
+	public void setPatientEmail(String patientEmail) {
+		this.patientEmail = patientEmail;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Patient patient)) return false;
+		return Objects.equals(getPatientID(), patient.getPatientID()) && Objects.equals(getPatientName(), patient.getPatientName()) && Objects.equals(getPatientSurname(), patient.getPatientSurname()) && Objects.equals(getTaxCode(), patient.getTaxCode()) && Objects.equals(getPatientPhoneNumber(), patient.getPatientPhoneNumber()) && Objects.equals(getPatientEmail(), patient.getPatientEmail());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getPatientID(), getPatientName(), getPatientSurname(), getTaxCode(), getPatientPhoneNumber(), getPatientEmail());
+	}
 }
+
