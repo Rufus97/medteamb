91f33cc9de613ef6a166c5a3b6af760fe3e2e356
dmanu <dmanu2804@gmail.com>
2024-04-19 22:34:03 +0200

    feat(doc-service) doc-servicedoc-service

--- src/main/java/com/medteamb/medteamb/service/DoctorService.java
+++ src/main/java/com/medteamb/medteamb/service/DoctorService.java
@@ -1,11 +1,20 @@
 package com.medteamb.medteamb.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
-import com.medteamb.medteamb.repository.DoctorRepository;
+import com.medteamb.medteamb.service.dto.doctor.DoctorRequestDTO;
 
 @Service
-public class DoctorService {
-DoctorRepository doctorRepo;
+public interface DoctorService {
+
+	public DoctorRequestDTO saveDoctor (DoctorRequestDTO docDto);
+	public List<DoctorRequestDTO> showAllDocs();
+	public Optional<DoctorRequestDTO> findDocById(Integer doctorID);
+	public Boolean exists(Integer doctorID);
+	public void deleteDoc(Integer doctorID);
+
 
 }
