815d54e0118019d1d5feb633f95f984f5d077391
Rufus97 <manuelgomez@outlook.it>
2024-04-19 18:59:53 +0200

    feature(doctor): initialization of crud , added only create and read

--- src/main/java/com/medteamb/medteamb/repository/DoctorRepository.java
+++ src/main/java/com/medteamb/medteamb/repository/DoctorRepository.java
@@ -1,12 +1,9 @@
 package com.medteamb.medteamb.repository;
 
+import com.medteamb.medteamb.model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.medteamb.medteamb.model.Doctor;
-
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
-
-	
 }
--- src/main/java/com/medteamb/medteamb/service/DoctorService.java
+++ src/main/java/com/medteamb/medteamb/service/DoctorService.java
@@ -1,11 +1,35 @@
 package com.medteamb.medteamb.service;
 
+import com.medteamb.medteamb.model.Doctor;
+import com.medteamb.medteamb.repository.DoctorRepository;
+import com.medteamb.medteamb.service.dto.doctor.DoctorMapper;
+import com.medteamb.medteamb.service.dto.doctor.DoctorRequestDTO;
+
 import org.springframework.stereotype.Service;
 
-import com.medteamb.medteamb.repository.DoctorRepository;
+import java.util.List;
 
 @Service
 public class DoctorService {
-DoctorRepository doctorRepo;
+
+	private DoctorRepository doctorRepository;
+	private final DoctorMapper doctorMapper;
+
+	public DoctorService(DoctorRepository doctorRepository) {
+		this.doctorMapper = new DoctorMapper();
+		this.doctorRepository = doctorRepository;
+	}
+
+	// CREATE
+	public DoctorRequestDTO newDoctor(DoctorRequestDTO newDoctorDTO) {
+		Doctor doctor = doctorMapper.mapFrom(newDoctorDTO);
+		return doctorMapper.mapTo(doctorRepository.save(doctor));
+	}
+
+	// READ
+	public List<DoctorRequestDTO> getAllDoctors() {
+		List<Doctor> doctors = doctorRepository.findAll();
+		return doctors.stream().map(doctorMapper::mapTo).collect(Collectors.toList());
+	}
 
 }
