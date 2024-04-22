f0b79f4402ec0a11b3914d13ae5a310c20126a77
Marija Milasinovic <30419423+mmilasi@users.noreply.github.com>
2024-04-22 11:45:07 +0200

    feature(controllers): typo fixes
    - fixed path naming to match indications in announcement channel on discord in secretary and patient controllers
    - renamed patientController class from "test"

---
+++ src/main/java/com/medteamb/medteamb/controller/PatientController.java
@@ -0,0 +1,60 @@
+package com.medteamb.medteamb.controller;
+
+import com.medteamb.medteamb.model.Appointment;
+import com.medteamb.medteamb.model.Doctor;
+import com.medteamb.medteamb.model.Patient;
+
+import com.medteamb.medteamb.service.DoctorService;
+import com.medteamb.medteamb.service.PatientService;
+
+import com.medteamb.medteamb.service.ResponseHandler.PatientResponse.PatientResponse;
+import com.medteamb.medteamb.service.dto.doctor.DoctorRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/patients")
+public class PatientController {
+    @Autowired
+    private PatientService service;
+
+    @Autowired
+    private DoctorService serviceDoc;
+
+
+    @PostMapping("/create")
+    public PatientResponse savePatient(@RequestBody Patient patient){
+        return service.newPatient(patient);
+    }
+    @DeleteMapping("/delete/{patientID}")
+    public PatientResponse deletePatientById(@RequestParam Integer patientID){
+        return service.deletePatientById(patientID);
+    }
+    @PutMapping("/update/{patientID}")
+    public PatientResponse updatePatientById(@RequestBody Patient newPatient, @PathVariable Integer patientID){
+        return service.updatePatientById(newPatient, patientID);
+    }
+    @GetMapping("/get/{patientID}")
+    public PatientResponse getPatient(@RequestParam Integer patientID){
+        return service.getPatient(patientID);
+    }
+    @GetMapping("/getAll")
+    public Iterable<Patient> getPatientsIds(@RequestBody Iterable<Integer> patientIDs){
+        return service.getPatientsByIds(patientIDs);
+    }
+    @GetMapping("/get/{patientID}/appointments")
+    public Iterable<Appointment> getAllAppointments(@RequestParam Integer patientID){
+        return service.getAllAppointment(patientID);
+    }
+
+
+    // perche' c'e' un doctor in questo controller?
+
+    @PostMapping("/doctor")
+    public DoctorRequestDTO postDoctor(@RequestBody DoctorRequestDTO docDto){
+        return serviceDoc.saveDoctor(docDto);
+    }
+
+
+
+}
